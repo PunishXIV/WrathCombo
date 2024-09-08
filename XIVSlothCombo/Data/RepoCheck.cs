9e2f802bfda5ab263120d664b6f893c8d340f2c7
Ethan Henderson <ethan@zbee.codes>
2024-09-07 22:22:42 -0600

    Clean up RepoCheck, - Check for forks of Wrath and Sloth - Give the currently-loaded manifest's repoURL instead of null, regardless of its name - Reduce duplicate null checks - Wrap deserialization in a try so bad json doesn't crash the plugin

--- XIVSlothCombo/Data/RepoCheck.cs
+++ XIVSlothCombo/Data/RepoCheck.cs
@@ -1,4 +1,5 @@
-﻿using ECommons.DalamudServices;
+﻿using System.Collections.Generic;
+using ECommons.DalamudServices;
 using Newtonsoft.Json;
 using System.IO;
 
@@ -14,31 +15,51 @@ namespace XIVSlothCombo.Data
         public static RepoCheck? FetchCurrentRepo()
         {
             FileInfo? f = Svc.PluginInterface.AssemblyLocation;
-            var manifest = Path.Join(f.DirectoryName, "XIVSlothCombo.json");
 
-            if (File.Exists(manifest))
+            List<string> listOfNamesToCheck = [
+                "XIVSlothCombo",
+                "WrathCombo",
+            ];
+
+            // Iterate over each name in the list
+            foreach (string name in listOfNamesToCheck)
             {
-                RepoCheck? repo = JsonConvert.DeserializeObject<RepoCheck>(File.ReadAllText(manifest));
-                return repo;
+                // Check if a manifest of this name exists
+                var manifest = Path.Join(f.DirectoryName, Svc.PluginInterface.InternalName + ".json");
+                if (!File.Exists(manifest)) continue;
+
+                // Load the manifest
+                try
+                {
+                    RepoCheck? repo = JsonConvert.DeserializeObject<RepoCheck>(File.ReadAllText(manifest));
+
+                    // Check if we were able to read the manifest and its repo URL
+                    if (repo?.InstalledFromUrl is null) continue;
+
+                    return repo;
+                }
+                catch { }
             }
-            else
+
+            // Default to the loaded manifest, whatever it was called
+            var defaultRepoCheck = new RepoCheck
             {
-                return null;
-            }
+                InstalledFromUrl = Svc.PluginInterface.Manifest.RepoUrl
+            };
+            return defaultRepoCheck;
 
         }
 
         public static bool IsFromSlothRepo()
         {
             RepoCheck? repo = FetchCurrentRepo();
-            if (repo is null) return false;
 
-            if (repo.InstalledFromUrl is null) return false;
+            if (repo?.InstalledFromUrl is null) return false;
 
             if (repo.InstalledFromUrl == "https://raw.githubusercontent.com/Nik-Potokar/MyDalamudPlugins/main/pluginmaster.json")
                 return true;
-            else
-                return false;
+
+            return false;
         }
     }
 }
