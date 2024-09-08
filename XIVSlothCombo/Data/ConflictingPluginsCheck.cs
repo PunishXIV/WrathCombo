23b7b12c94b199f08ea4e87ec8dd644410f8b3a6
Ethan Henderson <ethan@zbee.codes>
2024-09-07 23:39:01 -0600

    Initial `ConflictingPluginsCheck`

---
+++ XIVSlothCombo/Data/ConflictingPluginsCheck.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using ECommons.DalamudServices;
+
+namespace XIVSlothCombo.Data;
+
+public static class ConflictingPluginsCheck
+{
+    private static string[] conflictingPluginsNames = new string[]
+    {
+        "xivcombo",
+        "xivcomboexpanded",
+        "xivcomboexpandedest",
+        "xivcombovx",
+    };
+
+    public static string[]? TryGetConflictingPlugins()
+    {
+        var conflictingPlugins = Svc.PluginInterface.InstalledPlugins
+            .Where(x => conflictingPluginsNames.Contains(x.InternalName.ToLower()))
+            .Select(x => $"{x.InternalName}({x.Version})")
+            .ToArray();
+
+        return conflictingPlugins.Length == 0 ? null : conflictingPlugins;
+    }
+}
