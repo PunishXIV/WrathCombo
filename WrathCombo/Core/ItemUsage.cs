a2484ef9ce78af99c01604b68f17fd06475ec886
Ethan Henderson <ethan@zbee.codes>
2025-10-06 11:49:00 -0600

    feat(ItemUsage): Change extensions to work off of presets

--- WrathCombo/Core/ItemUsage.cs
+++ WrathCombo/Core/ItemUsage.cs
@@ -1,4 +1,11 @@
+#region
+
 using System;
+using ECommons.Logging;
+using WrathCombo.Combos.PvE;
+using WrathCombo.CustomComboNS;
+
+#endregion
 
 namespace WrathCombo.Core;
 
@@ -24,23 +31,49 @@ public enum HealingPotion
 
 public class ItemUsage : IDisposable
 {
-    
-    private class ItemUse
-    {
-    
-    }
-
     public void Dispose()
     {
         // TODO release managed resources here
+    }
+
+    private class ItemUse
+    {
     }
 }
 
 internal static class ItemUsageExtensions
 {
-    internal static uint UsePotion
-        (this uint action)
+    /// <summary>
+    /// </summary>
+    /// <example>
+    ///     Just return like this in your combo:
+    ///     <code>
+    ///     if (timeToPot)
+    ///       return this.UsePotion();
+    ///     </code>
+    ///     The <c>this</c> is your current combo (so this can access the
+    ///     <c>Preset</c> property), and the <c>UsePotion</c> method will return
+    ///     <see cref="All.Item" />, which will make the combo system use the
+    ///     potion defined on the preset.
+    /// </example>
+    /// <returns>
+    ///     <see cref="All.Item" />
+    /// </returns>
+    internal static uint UsePotion(this CustomCombo combo)
     {
-        return 0;
+        var preset = combo.Preset;
+
+        return All.Item;
+    }
+
+    internal static uint UsePotion(this uint actionID, Preset preset)
+    {
+        if (actionID is not All.Item)
+        {
+            PluginLog.Error("bad!");
+            return All.Item;
+        }
+
+        return All.Item;
     }
 }
