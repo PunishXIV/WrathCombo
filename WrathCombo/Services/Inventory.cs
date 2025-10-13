bcfff308e303009dc2e317efed28b552c191201a
Ethan Henderson <ethan@zbee.codes>
2025-10-13 11:32:20 -0600

    feat(Inventory): Initial implementation of an item list
    Contains only items we could care about.

--- WrathCombo/Services/Inventory.cs
+++ WrathCombo/Services/Inventory.cs
@@ -7,54 +7,72 @@ using Dalamud.Game;
 using ECommons.DalamudServices;
 using ECommons.ExcelServices;
 using ECommons.ExcelServices.Enums;
+using ECommons.Logging;
 using FFXIVClientStructs.FFXIV.Client.Game;
 using Lumina.Excel.Sheets;
-using Status = FFXIVClientStructs.FFXIV.Client.Game.Status;
 
 #endregion
 
 namespace WrathCombo.Services;
 
-public static class Inventory
+public class Inventory
 {
-    private static readonly FrozenDictionary<uint, Item> ItemSheet =
+    private readonly unsafe InventoryManager* _manager =
+        InventoryManager.Instance();
+
+    private readonly FrozenDictionary<uint, Item> _itemSheet =
         Svc.Data.GetExcelSheet<Item>(ClientLanguage.English)
             .Where(IsItemWeCareAbout)
             .ToFrozenDictionary(i => i.RowId);
 
-    private static unsafe InventoryManager* Manager => InventoryManager.Instance();
-
-    public static unsafe void t()
+    public unsafe void DebugInventory()
     {
-        var l = Manager->Inventories;
-        var o = l->IsLoaded;
+        PluginLog.Debug(
+            "[InventoryService] " +
+            "inventory loaded: " + _manager->Inventories->IsLoaded +
+            ", found items: " + _itemSheet.Count +
+            ", item names: " + string.Join(", ", _itemSheet
+                .Select(x => x.Value.GetName())) +
+            ", quantity pot: " + _manager->GetInventoryItemCount(38956u.HQ(), true)
+        );
     }
 
     #region .Where() Methods
+
+    private static bool IsPhoenixDown(Item item) =>
+        item.ItemUICategory.RowId == (uint)ItemUICategoryEnum.Medicine &&
+        item.ItemAction is { IsValid: true, RowId: (uint)ItemAction.PhoenixDown };
 
     private static bool IsMedicine(Item item) =>
         item.ItemUICategory.RowId == (uint)ItemUICategoryEnum.Medicine &&
         item.ItemAction.IsValid &&
         item.GivesStatus(ItemStatus.Medicated);
 
-    private static bool IsItemWeCareAbout(Item item)
-    {
-        if (!IsMedicine(item))
-            return false;
-
-        return true;
-    }
+    private static bool IsItemWeCareAbout(Item item) =>
+        IsPhoenixDown(item) ||
+        (IsMedicine(item) &&
+         (IsStatPotion(item) ||
+          IsHPPotion(item) ||
+          IsMPPotion(item)));
 
     private static bool IsStatPotion(Item item) =>
         (item.BaseParams() ?? []).Any(param =>
-            param == (uint)BaseParamEnum.Strength ||
-            param == (uint)BaseParamEnum.Dexterity ||
-            param == (uint)BaseParamEnum.Vitality ||
-            param == (uint)BaseParamEnum.Intelligence ||
-            param == (uint)BaseParamEnum.Mind);
+            param is (uint)BaseParamEnum.Strength or
+                (uint)BaseParamEnum.Dexterity or
+                (uint)BaseParamEnum.Vitality or
+                (uint)BaseParamEnum.Intelligence or
+                (uint)BaseParamEnum.Mind);
+
+    private static bool IsHPPotion(Item item) =>
+        (item.BaseParams() ?? []).Any(param => param == (uint)BaseParamEnum.HP);
+
+    private static bool IsMPPotion(Item item) =>
+        (item.BaseParams() ?? []).Any(param => param == (uint)BaseParamEnum.MP);
 
     #endregion
 }
+
+#region Static Data
 
 internal enum ItemStatus
 {
@@ -62,8 +80,17 @@ internal enum ItemStatus
     Medicated = 49,
 }
 
+internal enum ItemAction
+{
+    PhoenixDown = 44,
+}
+
+#endregion
+
 internal static class ItemExtensions
 {
+    private static readonly Dictionary<uint, uint[]?> SavedBaseParams = [];
+
     internal static bool GivesStatus(this Item item, ItemStatus status) =>
         item.ItemAction.IsValid &&
         (item.ItemAction.Value.Data[(int)DataKeys.Status] == (ushort)status ||
@@ -76,21 +103,19 @@ internal static class ItemExtensions
 
         if (hq is null or false &&
             Svc.Data.GetExcelSheet<ItemFood>()
-            .TryGetRow(item.ItemAction.Value.Data[(int)DataKeys.ItemFoodRowId],
-                out var row))
+                .TryGetRow(item.ItemAction.Value.Data[(int)DataKeys.ItemFoodRowId],
+                    out var row))
             return row;
 
         if (hq is null or true &&
             Svc.Data.GetExcelSheet<ItemFood>()
-            .TryGetRow(item.ItemAction.Value.DataHQ[(int)DataKeys.ItemFoodRowId],
-                out var rowHQ))
+                .TryGetRow(item.ItemAction.Value.DataHQ[(int)DataKeys.ItemFoodRowId],
+                    out var rowHQ))
             return rowHQ;
 
         return null;
     }
 
-    private static readonly Dictionary<uint, uint[]?> SavedBaseParams = [];
-    
     internal static uint[]? BaseParams(this Item item, bool? hq = null)
     {
         var itemID = hq is true ? item.RowId.HQ() : item.RowId;
@@ -105,9 +130,11 @@ internal static class ItemExtensions
 
         return foundParams;
     }
-    
+
     internal static uint HQ(this uint itemID) =>
         itemID + 1_000_000;
+
+    #region Static Data
 
     private enum DataKeys
     {
@@ -115,4 +142,6 @@ internal static class ItemExtensions
         ItemFoodRowId = 1,
         StatusDuration = 2,
     }
+
+    #endregion
 }
