2fd90aa7180fee074f64f1dbf0ba675d5e5f1c13
Tartarga <109563717+Tartarga@users.noreply.github.com>
2026-09-07 08:44:18 -0500

    Using data from BMR & Youtube video confirmation/pairs

--- WrathCombo/Data/BattleData/BattleData_7.0_DT.cs
+++ WrathCombo/Data/BattleData/BattleData_7.0_DT.cs
@@ -183,21 +183,21 @@ namespace WrathCombo.Data.BattleData
                     }.ToFrozenSet();
 
                     break;
-                
+
                 case 1346: //The Forked Tower: Magic
-                    // Two-headed Aevis Blue Head = 14491
-                    // Two-headed Aevis Green Head = 14490
-                    _invincibleCheck = (tar, _, _) =>
+                    // Two-headed Aevis Green Head = 19474
+                    // Two-headed Aevis Blue Head = 19475
+                    _invincibleCheck = (_, targetID, _) =>
                     {
-                        if (tar?.NameId is 14491 or 14490)
+                        if (targetID is 19474 or 19475)
                         {
-                            if (HasStatusEffect(4192)) return Result(tar.NameId != 14491); // Two-headed Aevis Blue Head
-                            if (HasStatusEffect(4194)) return Result(tar.NameId != 14490); // Two-headed Aevis Green Head
+                            if (HasStatusEffect(4192)) return Result(targetID != 19474); // Epic Hero (α)  — Green
+                            if (HasStatusEffect(4194)) return Result(targetID != 19475); // Fated Hero (β) — Blue
                         }
                         return Invincible.False;
                     };
                     break;
-                
+
                 case 1363: // Dancing Mad (Ultimate)
                            // Chaos = 19508
                            // Exdeath = 19509
@@ -254,7 +254,7 @@ namespace WrathCombo.Data.BattleData
                     }.ToFrozenSet();
 
                     break;
-                
+
                 default:
                     dataLoaded = false;
                     break;
